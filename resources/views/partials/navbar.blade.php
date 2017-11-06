2690cbe536889ffef4fb6cfcdc8106fc2b375354
Stefano Ruth <stefano6262@gmail.com>
2017-11-06 11:25:03 +0100

    Some design

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="flex p-6 bg-blue items-center justify-between flex-wrap text-white select-none">
+    <div class="flex mr-6">
+        <span class="text-xl font-semibold">{{ config('app.name') }}</span>
+    </div>
+    <div class="flex-grow flex items-center">
+        <a href="#" class="inline-block text-white mr-4 no-underline hover:underline">Link 1</a>
+        <a href="#" class="inline-block text-white mr-4 no-underline hover:underline">Link 2</a>
+        <a href="#" class="inline-block text-white mr-4 no-underline hover:underline">Link 3</a>
+        <a href="#" class="inline-block text-white no-underline hover:underline">Link 4</a>
+    </div>
+</nav>
