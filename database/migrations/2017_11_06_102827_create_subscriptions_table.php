07b36256c7b6d7c7d18a1fd4f145191099a73fb1
Stefano Ruth <stefano6262@gmail.com>
2017-11-06 13:07:06 +0100

    Stripe integration

---
+++ database/migrations/2017_11_06_102827_create_subscriptions_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_billings', function ($table) {
+            $table->increments('id');
+            $table->integer('site_id')->unsigned();
+            $table->string('email');
+            $table->string('stripe_id')->nullable();
+            $table->string('card_brand')->nullable();
+            $table->string('card_last_four')->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('site_id')->references('id')->on('sites')->onDelete('cascade');
+        });
+
+        Schema::create('subscriptions', function ($table) {
+            $table->increments('id');
+            $table->integer('billing_id');
+            $table->string('name');
+            $table->string('stripe_id');
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_billings');
+        Schema::dropIfExists('subscriptions');
+    }
+}
