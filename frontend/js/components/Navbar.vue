229458f32a2a1d77cd0f38f4aeda050604d514a6
JonasQuist <Jonasquist96@gmail.com>
2017-11-15 14:29:33 +0100

    Changed navbar to be using bulma

--- frontend/js/components/Navbar.vue
+++ frontend/js/components/Navbar.vue
@@ -1,18 +1,40 @@
 <template>
-    <nav id="navbar" class="flex p-6 bg-blue items-center justify-between flex-wrap text-white select-none">
-        <div class="flex mr-6">
-            <span class="text-xl font-semibold">Whalecore</span>
+    <nav class="navbar is-unselectable is-light">
+        <div class="navbar-brand">
+            <a href="" class="navbar-item">
+                <img src="/hvalborg.png">
+            </a>
+            <div class="navbar-burger" v-on:click="mobileShow = !mobileShow" v-bind:class="{'is-active':mobileShow}">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </div>
-        <div class="flex-grow flex items-center">
-            <router-link to="/" class="inline-block text-white mr-4 no-underline hover:underline">Dashboard</router-link>
-            <router-link to="/pages" class="inline-block text-white mr-4 no-underline hover:underline">Pages</router-link>
-            <router-link to="/buckets" class="inline-block text-white mr-4 no-underline hover:underline">Buckets</router-link>
-            <router-link to="/templates" class="inline-block text-white mr-4 no-underline hover:underline">Templates</router-link>
-            <router-link to="#" class="inline-block text-white mr-4 no-underline hover:underline">Components</router-link>
-            <router-link to="#" class="inline-block text-white mr-4 no-underline hover:underline">Media Library</router-link>
-            <router-link to="#" class="inline-block text-white mr-4 no-underline hover:underline">Settings</router-link>
-            <router-link to="#" class="inline-block text-white mr-4 no-underline hover:underline">User</router-link>
-            <a href="/logout" class="inline-block text-white mr-4 no-underline hover:underline">Logout</a>
+        <div class="navbar-menu" v-bind:class="{'is-active':mobileShow}">
+            <div class="navbar-start">
+                <router-link to="/" class="navbar-item">Dashboard</router-link>
+                <router-link to="/pages" class="navbar-item">Pages</router-link>
+                <router-link to="/buckets" class="navbar-item">Buckets</router-link>
+                <router-link to="/templates" class="navbar-item">Templates</router-link>
+                <router-link to="#" class="navbar-item">Components</router-link>
+                <router-link to="#" class="navbar-item">Media Library</router-link>
+            </div>
+            <div class="navbar-end">
+                <router-link to="#" class="navbar-item">Settings</router-link>
+                <router-link to="#" class="navbar-item">User</router-link>
+                <a href="/logout" class="navbar-item">Logout</a>
+            </div>
         </div>
     </nav>
 </template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      mobileShow: false
+    };
+  }
+};
+</script>
