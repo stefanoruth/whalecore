611d2c4c35201d2a76b437c16d6aebd737d141c3
Stefano Ruth <stefano6262@gmail.com>
2017-11-15 18:09:16 +0100

    Save

--- frontend/js/content-builder/ContentEditor.vue
+++ frontend/js/content-builder/ContentEditor.vue
@@ -1,6 +1,12 @@
 <template>
-    <div v-if="content != null">
-        <content-field v-for="(field, key) in template" :key="key" :field="field" :indentifier="key"></content-field>
+    <div v-if="template.length > 0">
+        <pre style="float:left;">{{ template | pretty }}</pre>
+        <pre style="float:left;">{{ content | pretty }}</pre>
+        <div style="clear:both;"></div>
+        <form action="" method="post" v-on:submit.prevent="onSubmit">
+            <content-field v-for="(field, key) in template" :key="key" :field="field" :indentifier="key"></content-field>
+            <button type="submit">Submit</button>
+        </form>
     </div>
 </template>
 
@@ -8,21 +14,60 @@
     export default {
         data() {
             return {
-                content: null,
+                model: null,
                 template: [],
-                fields: [],
+                content: [],
+                form: null,
             }
         },
 
         mounted() {
             axios.get(route('pages.show', this.$route.params.id)).then(response => {
-                this.content = response.data;
+                this.model = response.data;
                 this.template = response.data.template.structure;
+
+
+                this.content = this.buildContent(this.template);
             });
         },
 
         methods: {
-            //
+            buildContent(template, parent) {
+                if (typeof parent !== 'undefined') {
+                    var data = {};
+                    for (var i = 0; i < template.length; i++) {
+                        if (template[i].fields.length > 0) {
+                            data[template[i].id] = this.buildContent(template[i].fields, template[i]);
+                        } else {
+                            data[template[i].id] = null;
+                        }
+                    }
+                } else {
+                    var data = [];
+                    for (var i = 0; i < template.length; i++) {
+                        var newField = {};
+
+                        if (template[i].fields.length > 0) {
+                            newField[template[i].id] = this.buildContent(template[i].fields, template[i]);
+                        } else {
+                            newField[template[i].id] = null;
+                        }
+
+                        data.push(newField);
+                    }
+                }
+
+                if (typeof parent !== 'undefined' && parent.type == 'repeater') {
+                    data = [data];
+                }
+
+                return data;
+            },
+
+            onSubmit(event) {
+                this.form = new FormData(event.target);
+                console.log(this.form);
+            }
         },
     }
 </script>
--- frontend/js/content-builder/ContentField.vue
+++ frontend/js/content-builder/ContentField.vue
@@ -1,16 +1,56 @@
 <template>
-    <div>
-        <h1>{{ indentifier }} - {{ field.title }}</h1>
-        <input type="text" :placeholder="field.placeholder">
+    <div style="border: 1px solid #000; padding: 5px;">
+        <h1>{{ field.title }}</h1>
+        <h2>
+            <span>{{ fieldKey }}</span>
+        </h2>
+        <input type="text" :name="fieldKey" :placeholder="field.placeholder">
         <p>{{ field.description }}</p>
-        <div>
-            <content-field v-for="(subField, key) in field.fields" :key="key" :field="subField" :indentifier="indentifier+'.'+key"></content-field>
+        <div v-if="field.type == 'repeater'"style="padding: 5px;">
+            <div v-for="i in 3">
+                <content-field v-for="(subField, key) in field.fields" :key="key" :field="subField" :parent="nameKey" :parentType="field.type" :indentifier="i"></content-field>
+            </div>
+        </div>
+        <div v-if="field.type == 'section'" style="padding: 5px;">
+            <content-field v-for="(subField, key) in field.fields" :key="key" :field="subField" :parent="nameKey" :parentType="field.type" :indentifier="key"></content-field>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['field', 'indentifier'],
+        props: [
+            'field',
+            'indentifier',
+            'parent',
+            'parentType',
+        ],
+
+        data() {
+            return {
+                input: null,
+            };
+        },
+
+        computed: {
+            fieldKey: function() {
+                if (typeof parent !== 'undefined') {
+                    return this.parent+this.nameKey;
+                }
+                return this.nameKey;
+            },
+
+            nameKey: function() {
+                if (typeof this.parent === 'undefined') {
+                    return this.indentifier+'['+this.field.id+']';
+                }
+
+                if (typeof this.parent !== 'undefined' && this.parentType == 'repeater') {
+                    return '['+this.indentifier+']['+this.field.id+']';
+                }
+
+                return '['+this.field.id+']';
+            }
+        }
     }
 </script>
