b092c89dfc2e807fec9e7465c5f0b32f9ed43b73
Stefano Ruth <stefano6262@gmail.com>
2017-11-16 13:03:37 +0100

    Add real section

--- frontend/js/views/Dashboard.vue
+++ frontend/js/views/Dashboard.vue
@@ -1,45 +1,45 @@
 <template>
-<section>
-<div class="container">
-<div class="columns is-multiline is-mobile">
-  <div v-for="project in projects" :key="project.id" class="column" @click="loginToProject(project.id)">
-   <div class="card card-equal-height">
-    <div class="card-content">
-    <div class="media">
-      <div class="media-left">
-        <figure class="image is-48x48">
-          <img src="/hvalborg.png" alt="Placeholder image">
-        </figure>
-      </div>
-      <div class="media-content">
-        <p class="title is-4">{{project.title}}</p>
-      </div>
-    </div>
-  </div>
-  </div> 
-  </div>
-  <div class="column">
-    <div class="card card-equal-height" @click="showModal = true">
+<div class="section">
+  <div class="container">
+    <div class="columns is-multiline is-mobile">
+      <div v-for="project in projects" :key="project.id" class="column" @click="loginToProject(project.id)">
+       <div class="card card-equal-height">
         <div class="card-content">
-          <div class="media"> 
-            <div class="media-content">
-              <p class="title is-4">New Project</p>
-            </div>
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="/hvalborg.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">{{project.title}}</p>
           </div>
         </div>
       </div>
-    </div>
-    <modal :show="showModal" @close="showModal = false">   
-       <div class="mb-4">
-          <label class="block text-grey-darker text-sm font-bold mb-2" for="email">Email</label>
-          <input id="modalInput" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" type="text" :class="{'border-red': errors != null}" v-model="newProjectTitle" autofocus placeholder="Title..">
-          <p class="text-red text-xs italic" v-if="errors != null">{{ errors.title[0] }}</p>
+      </div> 
+      </div>
+      <div class="column">
+        <div class="card card-equal-height" @click="showModal = true">
+            <div class="card-content">
+              <div class="media"> 
+                <div class="media-content">
+                  <p class="title is-4">New Project</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <button @click="newProject">Create</button>
-  </modal>
+        </div>
+        <modal :show="showModal" @close="showModal = false">   
+           <div class="mb-4">
+              <label class="block text-grey-darker text-sm font-bold mb-2" for="email">Email</label>
+              <input id="modalInput" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" type="text" :class="{'border-red': errors != null}" v-model="newProjectTitle" autofocus placeholder="Title..">
+              <p class="text-red text-xs italic" v-if="errors != null">{{ errors.title[0] }}</p>
+              </div>
+              <button @click="newProject">Create</button>
+      </modal>
+    </div>
+    </div>
 </div>
-</div>
-</section>
 </template>
 <script>
 export default {
