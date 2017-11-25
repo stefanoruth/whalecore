04083b69641f4cfd06b7c1d5412a39b8c8499f9a
JonasQuist <Jonasquist96@gmail.com>
2017-11-25 17:10:04 +0100

    #CreateATemplateMaMan

--- frontend/js/views/Templates.vue
+++ frontend/js/views/Templates.vue
@@ -18,20 +18,75 @@
                 </tr>
             </tbody>
         </table>
+        <div class="is-clearfix">
+            <button class="button is-success is-small is-pulled-right" @click="showModal = true" >Create</button>            
+        </div>
+        <modal :show="showModal" @close="closeModal">   
+            <h1 slot="title">New Template</h1>            
+            <div>
+                <div class="field">
+                    <label class="label" for="title">Template title</label>
+                    <div class="control">
+                        <input class="input" type="text" :class="{'border-red': errors != null}" v-model="newTemplateTitle" autofocus placeholder="Title..">
+                        <p class="help is-danger" v-if="errors != null">{{ errors.title[0] }}</p>           
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="" class="label"></label>
+                    <div class="control">
+                        <div class="select is-fullwidth">
+                            <select v-model="newTemplateType">
+                                <option value="" selected disabled>Choose one</option>
+                               <option v-for="Type in templateTypes" :key="Type.id" :value="Type.id">{{Type.name}}</option>
+                            </select>
+                        </div>
+                        <p class="help is-danger" v-if="errors != null">{{ errors.type[0] }}</p>                                   
+                    </div>
+                </div>
+            </div>
+            <button class="button is-primary is-medium" slot="footer" @click="newTemplate">Create</button>
+        </modal>
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            templates: []
-        };
-    },
-    mounted() {
-        axios.get(route("templates.index")).then(response => {
-            this.templates = response.data.data;
+  data() {
+    return {
+      templates: [],
+      templateTypes: [],
+      showModal: false,
+      newTemplateTitle: null,
+      newTemplateType: "",
+      errors: null
+    };
+  },
+  mounted() {
+    axios.get(route("templates.index")).then(response => {
+      this.templates = response.data.data;
+    }),
+      axios.get(route("types.index")).then(response => {
+        this.templateTypes = response.data;
+      });
+  },
+  methods: {
+    newTemplate() {
+      axios
+        .post(route("templates.store"), {
+          title: this.newTemplateTitle,
+          type: this.newTemplateType
+        })
+        .then(response => {
+            this.$router.push("/templates/" + response.data.data.id);
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
         });
+    },
+    closeModal(){
+        this.showModal = false,
+        this.errors = null
     }
+  }
 };
 </script>
