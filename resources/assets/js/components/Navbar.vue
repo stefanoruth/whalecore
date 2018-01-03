005c5cf59fdc99f53c23f1ed8fd234eaa526682d
Stefano Ruth <stefano6262@gmail.com>
2018-01-03 16:55:43 +0100

    Update menu

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -15,15 +15,19 @@
     <nav v-else class="bg-blue text-white flex flex-col border-r border-blue-lighter select-none flex-no-shrink">
        <div class="flex flex-col items-center p-4">
            <img class="w-16 h-16 inline bg-white p-2 rounded-full mb-4" src="/logo.png">
-           <div class="rounded-lg bg-blue-light relative py-2 pr-6 pl-4 cursor-pointer w-full">
+           <div class="rounded-lg bg-blue-light relative py-2 pr-6 pl-4 cursor-pointer w-full group">
                <div class="text-white">Project: <span v-if="project != null">{{ project.title }}</span></div>
                <div class="pointer-events-none absolute pin-y pin-r flex items-center px-1 text-white">
                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                 </div>
-                <li v-for="item in projects" :key="item.id">{{ item.title }}</li>
+                <div class="absolute pin-to pin-l w-full hidden group-hover:block bg-white py-2 rounded shadow">
+                    <div v-for="item in projects" :key="item.id" class="mb-1">
+                        <a class="text-black px-2 block" @click="loginToProject(item.id)">{{ item.title }}</a>
+                    </div>
+                </div>
            </div>
        </div>
-       <div class="py-8 pl-6 flex-1 flex flex-col justify">
+       <div class="py-8 pl-6 flex-1 flex flex-col">
            <div class="uppercase mb-4 pl-2">Main</div>
            <ul class="list-reset mb-8">
                 <li class="mb-2">
@@ -43,6 +47,12 @@
                         <svg class="fill-current text-white h-4 w-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1z"/></svg>
                         <span>Media Library</span>
                     </router-link>
+                </li>
+                <li class="mb-2">
+                    <a href="/logout" class="flex text-white no-underline hover:bg-blue-dark py-2 block pl-2 pr-8 rounded-lg rounded-r-none">
+                        <svg class="fill-current text-white h-4 w-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M11 7l1.44 2.16c.31.47 1.01.84 1.57.84H17V8h-3l-1.44-2.16a5.94 5.94 0 0 0-1.4-1.4l-1.32-.88a1.72 1.72 0 0 0-1.7-.04L4 6v5h2V7l2-1-3 14h2l2.35-7.65L11 14v6h2v-8l-2.7-2.7L11 7zm1-3a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/></svg>
+                        <span>Logout</span>
+                    </a>
                 </li>
            </ul>
            <div class="uppercase mb-4 pl-2">Help</div>
@@ -90,6 +100,7 @@
                 axios.post(route('tenant.store'), {projectId: id}).then(response => {
                     this.project = response.data.data;
                     this.$router.push('/content');
+                    window.location.reload();
                 });
             },
         },
