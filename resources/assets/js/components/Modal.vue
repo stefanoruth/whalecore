85bd1a3bf9f9b63bd604b9248b75c2797379f8e0
Stefano Ruth <stefano6262@gmail.com>
2018-01-04 16:47:08 +0100

    Remove rounded from modal

--- resources/assets/js/components/Modal.vue
+++ resources/assets/js/components/Modal.vue
@@ -2,7 +2,7 @@
     <div class="fixed pin flex">
         <div class="bg-blue absolute pin opacity-75" @click="$emit('close')"></div>
         <div class="flex flex-1 justify-center items-center" @click.stop>
-            <div class="bg-white shadow rounded relative">
+            <div class="bg-white border shadow relative">
                 <header class="border-b text-xl py-2 px-4" v-if="$slots.header != null">
                     <slot name="header"></slot>
                 </header>
