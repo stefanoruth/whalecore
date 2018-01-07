9881f11b1c0c5d780b5a29db2ed40da4267e1ea8
JonasQuist <Jonasquist96@gmail.com>
2018-01-07 15:31:15 +0100

    Text changes ;)

--- resources/views/mail-signup.blade.php
+++ resources/views/mail-signup.blade.php
@@ -9,10 +9,10 @@
                     <img class="h-16 w-16 mr-4" src="{{ asset('logo.png') }}" alt="Whalecore Logo">
                     <span class="text-xl">Whalecore</span>
                 </div>
-                <div class="text-3xl py-6">Lorem ipsum dolor sit amet.</div>
-                <div class="mb-6 text-xl text-grey-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti impedit saepe magni maxime, quos voluptatibus accusantium a provident quibusdam vitae? Fugiat laborum fuga sapiente perspiciatis ut necessitatibus suscipit mollitia est!</div>
+                <div class="text-3xl py-6">Whoops! This is slightly embarrassing</div>
+                <div class="mb-6 text-xl text-grey-dark">We're just tidying up the last pieces and getting ready to help you all being amazing sailors!</div>
                 <label class="mb-8 block">
-                    <div class="mb-2 text-grey-dark">Sign up for the newsletter</div>
+                    <div class="mb-2 text-grey-dark">Sign up for our newsletter to stay up to date with the latest news</div>
                     <div class="flex">
                         <input class="px-3 py-2 text-xl text-grey-darker max-w-xs flex-1 shadow rounded-l-lg leading-normal" v-model="mail" type="text" placeholder="Enter your email" @keyup.enter="subscribe">
                         <button class="text-xl bg-blue appearance-none px-6 py-2 text-white hover:bg-blue-dark shadow rounded-r-lg leading-normal" @click="subscribe">Signup</button>
