d869001fa584c4a5338714d677e9fd742d9ccfa6
Stefano Ruth <stefano6262@gmail.com>
2018-01-07 15:06:18 +0100

    Darker text

--- resources/views/mail-signup.blade.php
+++ resources/views/mail-signup.blade.php
@@ -14,7 +14,7 @@
                 <label class="mb-8 block">
                     <div class="mb-2 text-grey-dark">Sign up for the newsletter</div>
                     <div class="flex">
-                        <input class="px-3 py-2 text-xl max-w-xs flex-1 shadow rounded-l-lg leading-normal" v-model="mail" type="text" placeholder="Enter your email" @keyup.enter="subscribe">
+                        <input class="px-3 py-2 text-xl text-grey-darker max-w-xs flex-1 shadow rounded-l-lg leading-normal" v-model="mail" type="text" placeholder="Enter your email" @keyup.enter="subscribe">
                         <button class="text-xl bg-blue appearance-none px-6 py-2 text-white hover:bg-blue-dark shadow rounded-r-lg leading-normal" @click="subscribe">Signup</button>
                     </div>
                     <div class="mt-2 text-blue" v-if="successMsg != null">@{{ successMsg }}</div>
