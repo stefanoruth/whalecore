19718d4abfcc52b5e8d7ae27fecee33850ad8c59
Stefano Ruth <stefanoruth@users.noreply.github.com>
2018-01-30 15:56:20 +0100

    Build secitons

--- resources/assets/js/views/Welcome.vue
+++ resources/assets/js/views/Welcome.vue
@@ -1,3 +1,139 @@
 <template>
-    <div>Hi</div>
+    <div>
+        <section class="bg-blue py-4">
+            <nav class="flex justify-between">
+                <div>
+                    <img src="/logo.png" alt="">
+                </div>
+
+                <ul class="flex list-reset">
+                    <li class="mr-4"><a class="block text-white no-underline px-6 py-3 hover:underline" href="#">Link 1</a></li>
+                    <li class="mr-4"><a class="block text-white no-underline px-6 py-3 hover:underline" href="#">Link 2</a></li>
+                    <li class="mr-4"><a class="block text-white no-underline px-6 py-3 hover:underline" href="#">Link 3</a></li>
+                </ul>
+            </nav>
+        </section>
+
+        <section class="bg-white py-8 px-4">
+            <div class="mx-auto max-w-2xl w-full text-center">
+                <div class="text-blue">CMS Flow</div>
+                <div class="text-black text-xl">How does our Content Management system work?</div>
+                <div></div>
+                <div class="text-grey-dark text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate dolore tenetur qui quasi minus accusamus inventore tempora, ut odit nam similique omnis quod facere ducimus pariatur aut sunt? Cupiditate.</div>
+                <div class="w-full">
+                    <img class="w-full rounded-lg" src="">
+                </div>
+            </div>
+        </section>
+
+        <section class="bg-blue text-white px-4 py-8">
+            <div class="mx-auto max-w-xs text-center">
+                <div>Reviews</div>
+                <div>See what others are saying!</div>
+                <div>
+                    <div class="bg-white p-8 w-full">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                    <div class="bg-white p-8 w-full">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                    <div class="bg-white p-8 w-full">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                </div>
+                <div class="flex justify-center">
+                    <span class="rounded-full h-2 w-2 border border-white bg-white"></span>
+                    <span class="rounded-full h-2 w-2 border border-white"></span>
+                    <span class="rounded-full h-2 w-2 border border-white"></span>
+                </div>
+            </div>
+        </section>
+
+        <section class="px-4 py-8">
+            <div class="flex">
+                <div class="flex-1">
+                    <img class="rounded-lg w-full" src="" alt="">
+                </div>
+                <div class="flex-1 flex justify-center items-center">
+                    <div class="text-center">
+                        <div class="text-blue">Title here</div>
+                        <div class="text-xl text-black">Some text here</div>
+                        <div></div>
+                        <div class="text-sm text-grey-dark">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, voluptatem adipisci beatae sunt nulla esse deserunt velit, dolores veniam quasi quibusdam veritatis sapiente rerum commodi necessitatibus voluptate magnam quos dolore.</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="px-4 py-8">
+            <div class="flex">
+                <div class="flex-1 flex justify-center items-center">
+                    <div class="text-center">
+                        <div class="text-blue">Title here</div>
+                        <div class="text-xl text-black">Some text here</div>
+                        <div></div>
+                        <div class="text-sm text-grey-dark">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, voluptatem adipisci beatae sunt nulla esse deserunt velit, dolores veniam quasi quibusdam veritatis sapiente rerum commodi necessitatibus voluptate magnam quos dolore.</div>
+                    </div>
+                </div>
+                <div class="flex-1">
+                    <img class="rounded-lg w-full" src="" alt="">
+                </div>
+            </div>
+        </section>
+
+        <section class="bg-grey-darker px-4 py-8">
+            <div class="mx-auto max-w-2xl">
+                <div class="mb-8 text-center">
+                    <div class="text-blue">Title here</div>
+                    <div class="text-white text-xl">Our Prices</div>
+                    <div></div>
+                    <div class="text-sm text-grey-lighter">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus placeat, nisi nemo nesciunt natus tempore optio consequatur officiis, itaque ipsa blanditiis animi fugit. Quaerat reprehenderit porro eos cum nemo necessitatibus.</div>
+                </div>
+                <div class="flex justify-between">
+                    <div class="rounded-md text-center bg-white">
+                        <div>
+                            <div>Beginner</div>
+                            <div>$10.99</div>
+                        </div>
+                        <div>
+                            <ul>
+                                <li>text 1</li>
+                                <li>text 2</li>
+                                <li>text 3</li>
+                            </ul>
+                            <div>
+                                <button class="bg-blue text-white rounded px-2 py-1">Get started</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="rounded-md text-center bg-white">
+                        <div>
+                            <div>Beginner</div>
+                            <div>$10.99</div>
+                        </div>
+                        <div>
+                            <ul>
+                                <li>text 1</li>
+                                <li>text 2</li>
+                                <li>text 3</li>
+                            </ul>
+                            <div>
+                                <button class="bg-blue text-white rounded px-2 py-1">Get started</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="rounded-md text-center bg-white">
+                        <div>
+                            <div>Beginner</div>
+                            <div>$10.99</div>
+                        </div>
+                        <div>
+                            <ul>
+                                <li>text 1</li>
+                                <li>text 2</li>
+                                <li>text 3</li>
+                            </ul>
+                            <div>
+                                <button class="bg-blue text-white rounded px-2 py-1">Get started</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
