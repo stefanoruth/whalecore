19ad28428b8cf60072713a795dcc941c8b121749
Stefano Ruth <stefanoruth@users.noreply.github.com>
2018-02-04 12:20:35 +0100

    Modal

--- resources/assets/js/components/Modal.vue
+++ resources/assets/js/components/Modal.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="fixed pin flex">
-        <div class="bg-primary absolute pin opacity-75" @click="$emit('close')"></div>
+        <div class="bg-grey absolute pin" @click="$emit('close')"></div>
         <div class="flex flex-1 justify-center items-center" @click.stop>
-            <div class="bg-white border shadow relative max-h-90 overflow-hidden flex flex-col">
-                <header class="border-b text-xl py-2 px-4" v-if="$slots.header != null">
+            <div class="bg-white shadow-lg rounded relative w-full max-w-sm max-h-90 overflow-hidden flex flex-col">
+                <header class="text-white bg-primary-darkest text-2xl p-4" v-if="$slots.header != null">
                     <slot name="header"></slot>
                 </header>
                 <main class="py-2 px-4 flex-1 overflow-hidden overflow-y-scroll">
